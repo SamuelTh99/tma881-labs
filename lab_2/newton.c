b0f27b060f82fd07e55442eabe8154690923acf4
Mats Högberg <mats@hgbrg.se>
2019-10-03 11:50:59 +0200

    .

--- lab_2/newton.c
+++ lab_2/newton.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// Implementation plan:
+// 1. Function for doing Newton's method for a single x value
+// 2. Function for doing Newton's method for a 1000x1000 grid using a single thread, writing result to file
+// 3. Multithreaded implementation of above
+// 4. Parse command line args
 
 int main() {
     printf("Hej\n");
     return 0;
 }
+
+
