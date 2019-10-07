850418131310e65a793c97ca71877e847739f9be
Mats Högberg <mats@hgbrg.se>
2019-10-07 15:13:00 +0200

    first version that loop through a 10 x 10 grid

--- lab_2/newton.c
+++ lab_2/newton.c
@@ -5,49 +5,84 @@
 #include <stdbool.h> 
 
 // Implementation plan:
-// 1. Function for doing Newton's method for a single x value
+// x. Function for doing Newton's method for a single x value
 // 2. Function for doing Newton's method for a 1000x1000 grid using a single thread, writing result to file
 // 3. Multithreaded implementation of above
 // 4. Parse command line args
 
-#define OUT_OF_BOUNDS 10000000000
-#define ERROR_MARGIN 0.001
-
-double complex roots[5];
-int num_roots;
-
-void print_complex_double(double complex d);
-int newton(double complex x, int d);
+void print_complex_double(double complex dbl);
+void find_roots();
+int newton(double complex x);
 bool illegal_value(double complex x);
 int get_nearby_root(double complex x);
-double complex next_x(double complex x, int d);
-double complex f(double complex x, int d);
-double complex f_deriv(double complex x, int d);
+double complex next_x(double complex x);
+double complex f(double complex x);
+double complex f_deriv(double complex x);
+
+#define OUT_OF_BOUNDS 10000000000
+#define ERROR_MARGIN 0.001
+#define X_MIN -2
+#define X_MAX 2
+
+int num_roots;
+double complex* roots;
+
+int picture_size;
+int d;
+
+int* results_values;
+int** results;
 
 int main() {
+    d = 5;
+    int l = 10;
+
+    num_roots = d;
+    roots = (double complex*) malloc(sizeof(double complex) * num_roots);
     roots[0] = 1;
     roots[1] = -cpow(-1, 0.2);
     roots[2] = cpow(-1, 0.4);
     roots[3] = -cpow(-1, 0.6);
     roots[4] = cpow(-1, 0.8);
-    num_roots = 5;
 
-    double complex x = 1 + I;
-    int d = 5;
+    picture_size = l;
+    
+    results_values = (int*) malloc(l * l * sizeof(int));
+    results = (int**) malloc(l * sizeof(int*));
+    for (size_t i = 0, j = 0; i < l; i++, j += l) {
+        results[i] = results_values + j;
+    }
 
-    int root = newton(x, d);
+    find_roots();
 
-    printf("%d\n", root);
-    print_complex_double(roots[root]);
+    for (int i = 0; i < l; i++) {
+        for (int j = 0; j < l; j++) {
+            printf("%d ", results[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
 
-void print_complex_double(double complex d) {
-    printf("%lf%+lfi\n", creal(d), cimag(d));
+void print_complex_double(double complex dbl) {
+    printf("%lf%+lfi\n", creal(dbl), cimag(dbl));
 }
 
-int newton(double complex x, int d) {
+void find_roots() {
+    double step_size = (X_MAX - X_MIN) / ((double) picture_size);
+    int i = 0, j = 0;
+    for (double re = X_MIN; re <= X_MAX; re += step_size, i++) {
+        for (double im = X_MIN; im <= X_MAX; im += step_size, j++) {
+            printf("find_roots - j: %d", j);
+            double complex x = re + im * I;
+            int root = newton(x);
+            results[i][j] = root;
+        }
+    }
+}
+
+int newton(double complex x) {
     for (int i = 0; ; i++) {
         if (illegal_value(x)) {
             break;
@@ -58,7 +93,7 @@ int newton(double complex x, int d) {
             return root;
         }
 
-        x = next_x(x, d);
+        x = next_x(x);
     }
 
     return -1;
@@ -83,14 +118,14 @@ int get_nearby_root(double complex x) {
     return -1;
 }
 
-double complex next_x(double complex x, int d) {
-    return x - f(x, d) / f_deriv(x, d);
+double complex next_x(double complex x) {
+    return x - f(x) / f_deriv(x);
 }
 
-double complex f(double complex x, int d) {
+double complex f(double complex x) {
     return cpow(x, d) - 1;
 }
 
-double complex f_deriv(double complex x, int d) {
+double complex f_deriv(double complex x) {
     return d * cpow(x, d - 1);
 }
