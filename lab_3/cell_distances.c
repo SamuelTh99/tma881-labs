0a13d0667ccbfaddf48fe166f4a6af49c5d4e297
Mats Högberg <mats@hgbrg.se>
2019-10-18 09:49:23 +0200

    Chunking

--- lab_3/cell_distances.c
+++ lab_3/cell_distances.c
@@ -5,7 +5,8 @@
 #include <getopt.h> 
 #include <omp.h> 
 
-#define CHUNK_SIZE 100000
+#define MAX_LINES 1000000
+#define CHUNK_SIZE 10000
 #define LINE_LENGTH 24
 #define MAX_DIST 3466
 #define FILENAME "cells"
@@ -36,15 +37,15 @@ int main(int argc, char* argv[]) {
     int num_threads = atoi(argv[1] + 2);
     omp_set_num_threads(num_threads);
 
-    // printf("reading file ...\n");
-    char* lines[CHUNK_SIZE];
+    printf("reading file ...\n");
+    char* lines[MAX_LINES];
     long num_coords = read_file(lines, filename);
 
-    // printf("parsing coords ...\n");
+    printf("parsing coords ...\n");
     struct coord coords[num_coords];
     parse_coords(coords, num_coords, lines);
 
-    // printf("computing distances ...\n");
+    printf("computing distances ...\n");
     long dist_counts[MAX_DIST];
     compute_distances(dist_counts, num_coords, coords);
 
@@ -62,7 +63,7 @@ long read_file(char* lines[], char* filename) {
 
     char line[LINE_LENGTH];
     long num_lines = 0;
-    while (fread(line, sizeof(char), LINE_LENGTH, fp) == LINE_LENGTH) {
+    while (fread(line, sizeof(char), LINE_LENGTH, fp) == LINE_LENGTH) { // TODO: no bufferoverflow -check
         lines[num_lines] = (char*) malloc(sizeof(char) * LINE_LENGTH);
         memcpy(lines[num_lines], line, sizeof(char) * LINE_LENGTH);
         num_lines++;
@@ -120,13 +121,35 @@ void compute_distances(long dist_counts[], long num_coords, struct coord coords[
     for (size_t dist = 0; dist < MAX_DIST; dist++) {
         dist_counts[dist] = 0;
     }
-    #pragma omp parallel for reduction(+:dist_counts[:MAX_DIST])
-    for (size_t i = 0; i < num_coords - 1; i++) {
-        for (size_t j = i + 1; j < num_coords; j++) {
-            struct coord c1 = coords[i];
-            struct coord c2 = coords[j];
-            short dist = compute_distance(c1, c2);
-            dist_counts[dist]++;
+    //#pragma omp parallel for reduction(+:dist_counts[:MAX_DIST])
+
+    // TODO: Make sure that we don't get rounding errors
+    size_t num_chunks = num_coords / CHUNK_SIZE + 1;
+
+    for (size_t chunk_i = 0; chunk_i < num_chunks; chunk_i++) {
+        size_t start_index_i = chunk_i * CHUNK_SIZE;
+
+        // Calculate distances between all coords in chunk i
+        for (size_t i1 = start_index_i; i1 < start_index_i + CHUNK_SIZE - 1 && i1 < num_coords; i1++) {
+            for (size_t i2 = i1 + 1; i2 < start_index_i + CHUNK_SIZE && i2 < num_coords; i2++) {
+                struct coord c1 = coords[i1];
+                struct coord c2 = coords[i2];
+                short dist = compute_distance(c1, c2);
+                dist_counts[dist]++;
+            }
+        }
+
+        for (size_t chunk_j = chunk_i + 1; chunk_j < num_chunks; chunk_j++) {
+            // Calculate distances between all coords in chunks i and j
+            for (size_t i = start_index_i; i < start_index_i + CHUNK_SIZE && i < num_coords; i++) {
+                size_t start_index_j = chunk_j * CHUNK_SIZE;
+                for (size_t j = start_index_j; j < start_index_j + CHUNK_SIZE && j < num_coords; j++) {
+                    struct coord c1 = coords[i];
+                    struct coord c2 = coords[j];
+                    short dist = compute_distance(c1, c2);
+                    dist_counts[dist]++;
+                }
+            }
         }
     }
 }
@@ -142,6 +165,8 @@ short compute_distance(struct coord c1, struct coord c2){
 void print_results(long dist_counts[]) {
     for (size_t i = 0; i < MAX_DIST; i++) {
         long count = dist_counts[i];
-        printf("%05.2f %ld\n", ((double) i) / 100.0, count);
+        if (count != 0) {
+            printf("%05.2f %ld\n", ((double) i) / 100.0, count);
+        }
     }
 }
