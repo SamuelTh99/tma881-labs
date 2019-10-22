77777cfa9e34637e5f00d941a189d3ce30229c07
Andréas Erlandsson <a.s.e.erlandsson@gmail.com>
2019-10-22 16:20:16 +0200

    done with lab 4

--- lab_4/heat_diffusion.c
+++ lab_4/heat_diffusion.c
@@ -1,114 +1,223 @@
+#define CL_TARGET_OPENCL_VERSION 220
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <getopt.h>
 #include <CL/cl.h>
 
-int main() {
+void read_input_file(char* filename, size_t* rows, size_t* cols, float** matrix);
+void init_cl(cl_context* context, cl_command_queue* command_queue, cl_program* program, cl_kernel* kernel);
+char* read_program();
+double average(float* matrix, size_t n);
+double average_diff(float* matrix, double avg, size_t n);
+void assert_success(cl_int error, char* msg);
+
+#define FILENAME "diffusion"
+
+int main(int argc, char* argv[]) {
+    // Parse cmd args.
+    float diffusion_constant = -1;
+    long iterations = -1;
+
+    int option; 
+    while ((option = getopt(argc, argv, "n:d:")) != -1) {
+        switch (option) {
+            case 'n':
+                iterations = atoi(optarg);
+                break;
+            case 'd':
+                diffusion_constant = atof(optarg);
+                break;
+            default:
+                printf("Usage: ./heat_diffusion -n<number of iterations> -d<diffusion constant>\n");
+                return 1;
+        }
+    }
+
+    char* filename = FILENAME;
+    if (argc == 4) {
+        filename = argv[3];
+    }
+
+    if (iterations  == -1 || diffusion_constant == -1){ 
+        printf("Usage: ./heat_diffusion -n<number of iterations> -d<diffusion constant> <filename>\n");
+        return 1;
+    }
+
+    // Read input file.
+    size_t rows, cols;
+    float* matrix;
+    read_input_file(filename, &rows, &cols, &matrix);
+    size_t n = rows * cols;
+
+    // Init OpenCL.
+    cl_context context;
+    cl_command_queue command_queue; 
+    cl_program program;
+    cl_kernel kernel;
+    init_cl(&context, &command_queue, &program, &kernel);
+
+    // Create and init buffer.
+    cl_int error;
+    cl_mem matrix_buffer = clCreateBuffer(context, CL_MEM_READ_WRITE, sizeof(float) * n, NULL, &error);
+    assert_success(error, "create cl buffer");
+    error = clEnqueueWriteBuffer(command_queue, matrix_buffer, CL_TRUE, 0, sizeof(float) * n, matrix, 0, NULL, NULL);
+    assert_success(error, "write to cl buffer");
+
+    // Execute kernel.
+    assert_success(clSetKernelArg(kernel, 0, sizeof(cl_mem), &matrix_buffer), "set kernel arg 0");
+    assert_success(clSetKernelArg(kernel, 1, sizeof(cl_uint), &n), "set kernel arg 1");
+    assert_success(clSetKernelArg(kernel, 2, sizeof(cl_uint), &cols), "set kernel arg 2");
+    assert_success(clSetKernelArg(kernel, 3, sizeof(cl_float), &diffusion_constant), "set kernel arg 3");
+    for (size_t i = 0; i < iterations; i++) {
+        const size_t global = n;
+        error = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL, (const size_t*) &global, NULL, 0, NULL, NULL);
+        assert_success(error, "enqueue kernel");
+    }
+
+    // Read output.
+    error = clEnqueueReadBuffer(command_queue, matrix_buffer, CL_TRUE, 0, n * sizeof(float), matrix, 0, NULL, NULL);
+    assert_success(error, "read from cl buffer");
+
+    // Wait for computation to finish.
+    error = clFinish(command_queue);
+    assert_success(error, "finish");
+
+    // Calculate & print averages.
+    double avg = average(matrix, n); 
+    double avg_diff = average_diff(matrix, avg, n);
+    printf("average: %lf\naverage absolute difference: %lf\n", avg, avg_diff);
+
+    // Release resources.
+    free(matrix);
+    clReleaseCommandQueue(command_queue);
+    clReleaseContext(context);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseMemObject(matrix_buffer);
+}
+
+void init_cl(cl_context* context, cl_command_queue* command_queue, cl_program* program, cl_kernel* kernel) {
     cl_int error;
 
     // Create platform.
     cl_platform_id platform_id;
     cl_uint nmb_platforms;
-    if (clGetPlatformIDs(1, &platform_id, &nmb_platforms) != CL_SUCCESS) {
-        printf( "cannot get platform\n" );
-        return 1;
-    }
+    error = clGetPlatformIDs(1, &platform_id, &nmb_platforms);
+    assert_success(error, "get platform id");
 
     // Create device.
     cl_device_id device_id;
     cl_uint nmb_devices;
-    if (clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1,
-        &device_id, &nmb_devices) != CL_SUCCESS) {
-        printf( "cannot get device\n" );
-        return 1;
-    }
+    error = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_GPU, 1, &device_id, &nmb_devices);
+    assert_success(error, "get device id");
 
     // Create context.
-    cl_context context;
     cl_context_properties properties[] = {
         CL_CONTEXT_PLATFORM,
         (cl_context_properties) platform_id,
         0
     };
-    context = clCreateContext(properties, 1, &device_id, NULL, NULL, &error);
+    *context = clCreateContext(properties, 1, &device_id, NULL, NULL, &error);
+    assert_success(error, "create context");
 
     // Create command queue.
-    cl_command_queue command_queue;
-    command_queue = clCreateCommandQueue(context, device_id, 0, &error);
-    if (error != CL_SUCCESS) {
-        printf("cannot create context\n");
-        return 1;
-    }
-
+    *command_queue = clCreateCommandQueueWithProperties(*context, device_id, 0, &error);
+    assert_success(error, "create command queue");
+    
     // Build kernel.
-    char* opencl_program_src =
-"__kernel void\n\
-    dot_prod_mul(\n\
-    __global const float * a,\n\
-    __global const float * b,\n\
-    __global float * c\n\
-    )\n\
-    {\n\
-    int ix = get_global_id(0);\n\
-    c[ix] = a[ix] * b[ix];\n\
-}";
-    cl_program program;
-    program = clCreateProgramWithSource(context,1,
-            (const char **) &opencl_program_src, NULL, &error);
+    char* opencl_program_src = read_program();
+    *program = clCreateProgramWithSource(*context, 1, (const char **) &opencl_program_src, NULL, &error);
+    free(opencl_program_src);
+    assert_success(error, "create program");
 
-    clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    error = clBuildProgram(*program, 0, NULL, NULL, NULL, NULL);
+    if (error != CL_SUCCESS) {
+        printf("cannot build program. log:\n");
 
-    cl_kernel kernel;
-    kernel = clCreateKernel(program, "dot_prod_mul", &error);
+        size_t log_size = 0;
+        error = clGetProgramBuildInfo(*program, device_id, CL_PROGRAM_BUILD_LOG, 0, NULL, &log_size);
+        assert_success(error, "get program build info");
 
-    // Create and init buffers.
-    const size_t ix_m = 1000;
-    cl_mem input_buffer_a, input_buffer_b, output_buffer_c;
-    input_buffer_a  = clCreateBuffer(context, CL_MEM_READ_ONLY,
-                        sizeof(float) * ix_m, NULL, &error);
-    input_buffer_b  = clCreateBuffer(context, CL_MEM_READ_ONLY,
-                        sizeof(float) * ix_m, NULL, &error);
-    output_buffer_c = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
-                        sizeof(float) * ix_m, NULL, &error);
+        char* log = calloc(log_size, sizeof(char));
+        if (log == NULL) {
+            printf("could not allocate memory\n");
+            exit(1);
+        }
 
-    float * a = malloc(ix_m*sizeof(float));
-    float * b = malloc(ix_m*sizeof(float));
-    for (size_t ix; ix < ix_m; ++ix) {
-        a[ix] = ix;
-        b[ix] = ix;
+        error = clGetProgramBuildInfo(*program, device_id, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
+        assert_success(error, "get program build info");
+        printf( "%s\n", log );
+
+        free(log);
+
+        exit(1);
     }
-    clEnqueueWriteBuffer(command_queue, input_buffer_a, CL_TRUE,
-        0, ix_m*sizeof(float), a, 0, NULL, NULL);
-    clEnqueueWriteBuffer(command_queue, input_buffer_b, CL_TRUE,
-        0, ix_m*sizeof(float), b, 0, NULL, NULL);
 
+    *kernel = clCreateKernel(*program, "heat_diffusion", &error);
+    assert_success(error, "create kernel");
+}
 
-    // Execute kernel.
-    clSetKernelArg(kernel, 0, sizeof(cl_mem), &input_buffer_a);
-    clSetKernelArg(kernel, 1, sizeof(cl_mem), &input_buffer_b);
-    clSetKernelArg(kernel, 2, sizeof(cl_mem), &output_buffer_c);
+char* read_program() {
+    FILE *f = fopen("heat_diffusion.cl", "r");
+    fseek(f, 0, SEEK_END);
+    long fsize = ftell(f);
+    fseek(f, 0, SEEK_SET);
 
-    const size_t global = ix_m;
-    clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
-        (const size_t *)&global, NULL, 0, NULL, NULL);
+    char* string = malloc(fsize + 1);
+    fread(string, 1, fsize, f);
+    fclose(f);
 
-    // Read output.
-    float * c = malloc(ix_m*sizeof(float));
-    clEnqueueReadBuffer(command_queue, output_buffer_c, CL_TRUE,
-        0, ix_m*sizeof(float), c, 0, NULL, NULL);
+    string[fsize] = 0;
 
-    clFinish(command_queue);
+    return string;
+}
 
-    printf("Square of %d is %f\n", 100, c[100]);
+void read_input_file(char* filename, size_t* rows, size_t* cols, float** matrix) {
+    FILE* f = fopen(filename, "r");
+    char* line;
+    int read = fscanf(f, "%zu %zu\n", cols, rows);
+    if (read < 2) {
+        printf("error read input file\n");
+    }
 
-    // Release resources.
-    free(a);
-    free(b);
-    free(c);
-    clReleaseCommandQueue(command_queue);
-    clReleaseContext(context);
-    clReleaseProgram(program);
-    clReleaseKernel(kernel);
-    clReleaseMemObject(input_buffer_a);
-    clReleaseMemObject(input_buffer_b);
-    clReleaseMemObject(output_buffer_c);
+    size_t n = (*rows) * (*cols);
+    float* m = (float*) malloc(sizeof(float) * n);
+    for (size_t i = 0; i < n; i++) {
+        m[i] = 0;
+    }
+
+    size_t row, col;
+    float val;
+    while ((read = fscanf(f, "%zu %zu %f\n", &col, &row, &val)) == 3) {
+        m[row * (*cols) + col] = val;
+    }
+
+    *matrix = m;
+}
+
+double average(float* matrix, size_t n) {
+  double avg = 0;
+  for (size_t i = 1; i <= n; i++) {
+    double x = matrix[i-1];
+    avg += (x - avg) / i;
+  }
+  return avg;
+}
+
+double average_diff(float* matrix, double avg, size_t n) {
+  double avg_diff = 0;
+  for (size_t i = 1; i <= n; i++) {
+    double x = abs(matrix[i-1] - avg);
+    avg_diff += (x - avg_diff) / i;
+  }
+  return avg_diff;
+}
+
+void assert_success(cl_int error, char* msg) {
+    if (error != CL_SUCCESS) {
+        printf("error: %s\n", msg);
+        exit(1);
+    }
 }
---
+++ lab_4/heat_diffusion.cl
@@ -0,0 +1,34 @@
+__kernel void
+    heat_diffusion(
+        __global float* m,
+        const uint n,
+        const uint row_len,
+        const float c
+    )
+{
+    size_t i = get_global_id(0);
+    
+    float sum = 0;
+
+    // left
+    if (i % row_len != 0) {
+        sum += m[i-1];
+    }
+    
+    // right
+    if (i % row_len != row_len - 1) {
+        sum += m[i+1];
+    }
+
+    // up
+    if (i - row_len >= 0) {
+        sum += m[i-row_len];
+    }
+
+    // down
+    if (i + row_len < n) {
+        sum += m[i+row_len];
+    }
+
+    m[i] = m[i] + c * (sum / 4 - m[i]);
+}
